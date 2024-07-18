119e367923efb1dffcb666988b512c93f6ceda05
Nandha-21 <nandhug2126@gmail.com>
2024-07-18 12:27:15 +0530

    project

---
+++ Arrayjavascript.js
@@ -0,0 +1,28 @@
+// Array types
+// First method
+ var arr =["nandha","sri","kumar"];
+ var no =[1,2,3,4,5];
+ console.log(arr);
+ console.table(arr);
+ console.log(no);
+ console.table(no);
+
+//  secong method
+ let a=[];
+ a[0]="nandha";
+a[1]="kumar";
+a[2]="linga";
+console.log(a);
+console.table(a);
+
+// third Method
+var a1 = new Array();
+a1[0]="values1";
+a1[1]="values2";
+a1[2]="values3";
+console.table(a1);
+console.log(typeof(a1));
+
+// method
+var a2= new Array("nandha","sri","kumar");
+console.log();
